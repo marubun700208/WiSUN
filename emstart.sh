774b87310656297645cdd88a62dfe1cc97770540
monikara <hiroki_mito@marubun.co.jp>
2026-05-15 15:19:39 +0900

    set ignore2

--- emstart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-/usr/bin/sudo /usr/bin/rm /tmp/sem.sock
-/usr/bin/sudo /usr/bin/rm /tmp/curr_pow.txt
-/home/mito/Wi-SUN_EnergyMeter/sem_app/bin/www &
-/home/mito/Wi-SUN_EnergyMeter/sem_com.py --delay 60
--- emstop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-kill -9 `pgrep -f "/home/mito/Wi-SUN_EnergyMeter/sem_app/bin/www"`
-kill -9 `pgrep -f "/home/mito/Wi-SUN_EnergyMeter/sem_com.py"`
-/usr/bin/sudo -f /usr/bin/rm /tmp/sem.sock
-/usr/bin/sudo -f /usr/bin/rm /tmp/curr_pow.txt
-
